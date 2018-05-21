f13fb8c30f01edbc033e0b6c93b1f7f60ce7ec9d
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2018-05-21 08:44:30 +0200

    Updated DiceArena to use HashMap intead of Array of int.

--- src/main/java/it/polimi/se2018/model/DiceArena.java
+++ src/main/java/it/polimi/se2018/model/DiceArena.java
@@ -1,18 +1,34 @@
 package it.polimi.se2018.model;
 
+import it.polimi.se2018.model.container.DiceContainer;
+import it.polimi.se2018.model.container.Die;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class DiceArena {
 
-    private int[] arena;
+    private HashMap<Integer, Die> arena;
+
+    private DiceContainer diceContainer;
+
+    public DiceArena(int numberOfDice) {
+        HashMap<Integer, Die> unrolledDice = diceContainer.getUnrolledDice();
+        ArrayList<Integer> unrolledDiceKeys = new ArrayList<>(unrolledDice.keySet());
+        Collections.shuffle(unrolledDiceKeys);
+        unrolledDiceKeys.subList(0, numberOfDice);
+    }
 
     /**
      *
      * @param arena list of dice Id to trhwo in the arena
      */
-    public void setArena(int[] arena) {
+    public void setArena(HashMap<Integer, Die> arena) {
         this.arena = arena;
     }
 
-    public int[] getArena() {
+    public HashMap<Integer, Die> getArena() {
         return arena;
     }
 }
