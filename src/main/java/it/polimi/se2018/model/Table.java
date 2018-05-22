55085642c148345c02891c5670c26b1139dd911b
Mattereteo <matteo3.montanari@mail.polimi.it>
2018-05-22 15:56:18 +0200

    Created constructor for private objective + constructor for table + load private objectives from JSON

--- src/main/java/it/polimi/se2018/model/Table.java
+++ src/main/java/it/polimi/se2018/model/Table.java
@@ -1,18 +1,18 @@
 package it.polimi.se2018.model;
 
 import it.polimi.se2018.model.container.DiceContainer;
-import it.polimi.se2018.model.objectives.PrivateObjective;
-import it.polimi.se2018.model.objectives.PublicObjective;
+import it.polimi.se2018.model.objectives.*;
 import it.polimi.se2018.model.rounds.RoundTrack;
+import it.polimi.se2018.model.toolcards.ToolCard;
 import it.polimi.se2018.model.toolcards.ToolCardContainer;
 import it.polimi.se2018.utils.Database;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Table {
 
     private ArrayList<PublicObjective> publicObjectives;
-
     private ToolCardContainer toolCardContainer;
 
     private DiceArena diceArena;
@@ -32,7 +32,7 @@ public class Table {
     private Scoreboard scoreboard;
 
 
-    /* TODO: COSTRUTTORE che crei ogni attributo e che setti i Players + controllare se tipo PLayer  ok
+    /* TODO: COSTRUTTORE che crei ogni attributo e che setti i Players + controllare se tipo PLayer  ok */
     public Table(int[] idPlayer, String[] name){
         for(int i = 0; i < idPlayer.length; i++) {
             this.players.add(new Player(idPlayer[i], name[i]))
@@ -51,7 +51,7 @@ public class Table {
         this.setPublicObjective();
         this.setToolCards();
     }
-    */
+
 
     public DiceArena getDiceArena() {
         return diceArena;
@@ -85,6 +85,39 @@ public class Table {
         return patternCards.get(i);
     }
 
+    private void setPublicObjective(){
+        ArrayList<Integer> publicObjectiveList = new ArrayList<>();
+        for (Integer i = 0; i < 10; i++)
+            publicObjectiveList.add(i);
+
+        Collections.shuffle(publicObjectiveList);
+
+        for(int j = 0; j < 3; j++)
+            switch(publicObjectiveList.get(j)) {
+                case 0: this.publicObjectives.add(new PublicObjective1(diceContainer)); break;
+                case 1: this.publicObjectives.add(new PublicObjective2(diceContainer)); break;
+                case 2: this.publicObjectives.add(new PublicObjective3(diceContainer)); break;
+                case 3: this.publicObjectives.add(new PublicObjective4(diceContainer)); break;
+                case 4: this.publicObjectives.add(new PublicObjective5(diceContainer)); break;
+                case 5: this.publicObjectives.add(new PublicObjective6(diceContainer)); break;
+                case 6: this.publicObjectives.add(new PublicObjective7(diceContainer)); break;
+                case 7: this.publicObjectives.add(new PublicObjective8(diceContainer)); break;
+                case 8: this.publicObjectives.add(new PublicObjective9(diceContainer)); break;
+                case 9: this.publicObjectives.add(new PublicObjective10(diceContainer)); break;
+                default: break;
+            }
+    }
+
+    private void setToolCards(){
+        ArrayList<Integer> toolCardsList = new ArrayList<>();
+        for (Integer i = 0; i < 10; i++)
+            toolCardsList.add(i);
+
+        Collections.shuffle(toolCardsList);
+
+        for(int j = 0; j < 3; j++)
+            this.toolCardContainer.setToolCardInPlay(j);
+    }
 
 
     /* TODO: toString */
