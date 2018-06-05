d78001d486087d03b0aa1230c5d5ca711e10255a
Alessandro Murace <alessandro.murace@gmail.com>
2018-06-05 16:38:54 +0200

    Created Test for class ToolCardContainer

---
+++ src/test/java/it/polimi/se2018/model/toolcards/TestToolCardContainer.java
@@ -0,0 +1,53 @@
+package it.polimi.se2018.model.toolcards;
+
+import it.polimi.se2018.model.container.DiceContainer;
+import it.polimi.se2018.utils.Database;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestToolCardContainer {
+
+    private Database database;
+    private DiceContainer diceContainer;
+    private ToolCardContainer toolCardContainer;
+
+    @Before
+    public void setUp() {
+        diceContainer = new DiceContainer();
+        database = new Database(diceContainer);
+        toolCardContainer = new ToolCardContainer(diceContainer);
+    }
+
+    @After
+    public void tearDown() {
+        diceContainer = null;
+        toolCardContainer = null;
+        diceContainer = null;
+    }
+
+    @Test
+    public void setToolCardInPlay_ControlName() {
+        toolCardContainer.setToolCardInPlay(0);
+        try {
+            assertEquals("Pinza Sgrossatrice", toolCardContainer.getToolCardInPlay().get(0).getName());
+        } catch (ToolCardNotInPlayException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void setToolCardInPlay_ControlDescription() {
+        toolCardContainer.setToolCardInPlay(0);
+        try {
+            assertEquals("Dopo aver scelto un dado, aumenta o diminuisci il valore del dado scelto di 1. Non puoi cambiare un 6 in 1 o un 1 in 6.", toolCardContainer.getToolCardInPlay().get(0).getDescription());
+        } catch (ToolCardNotInPlayException e) {
+            fail();
+        }
+    }
+
+    @Test (expected = java.lang.Exception.class)
+    public void setToolCardInPlay_ExceptionThrown() throws ToolCardNotInPlayException {
+        toolCardContainer.setToolCardInPlay(0);
+        toolCardContainer.getToolCardInPlay(2).getDescription();
+    }
+}
