a72d30a6e408205f207d060054a9dff9e4585a27
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2018-06-06 09:56:38 +0200

    Removed everything in network, defined ClientInterface and ServerInterface

--- src/main/java/it/polimi/se2018/network/ClientInterface.java
+++ src/main/java/it/polimi/se2018/network/ClientInterface.java
@@ -4,24 +4,23 @@ import it.polimi.se2018.model.events.ModelChangedMessage;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
+import java.util.List;
 
 public interface ClientInterface extends Remote {
 
     public void update(ModelChangedMessage modelChangedMessage) throws RemoteException;
 
+    public Integer getDieFromPatternCard() throws RemoteException;
 
-    public int getDieFromPatternCard() throws RemoteException;
+    public Integer getDieFromRoundTrack() throws RemoteException;
 
-    public int getDieFromRoundTrack() throws RemoteException;
-
-    public boolean getIncrementedValue() throws RemoteException;
+    public Boolean getIncrementedValue() throws RemoteException;
 
     public Integer[] getPositionInPatternCard() throws RemoteException;
 
-    public int getValueForDie() throws RemoteException;
+    public Integer getValueForDie() throws RemoteException;
 
     public String askForName() throws RemoteException;
 
-    public ArrayList<Integer> askForPatternCard() throws RemoteException;
+    public List<Integer> askForPatternCard() throws RemoteException;
 }
--- src/main/java/it/polimi/se2018/network/ServerInterface.java
+++ src/main/java/it/polimi/se2018/network/ServerInterface.java
@@ -5,10 +5,10 @@ import it.polimi.se2018.model.events.PlayerMessage;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface ServerInterface extends Remote {
+public interface ServerInterface<T> extends Remote {
 
     public void notify(PlayerMessage playerMessage) throws RemoteException;
 
-    public void addClient(ClientInterface clientInterface) throws RemoteException;
+    public void registerClient(T client, String name) throws RemoteException;
 
 }
