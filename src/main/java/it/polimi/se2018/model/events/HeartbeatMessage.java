e821a2dea7e7dafec3b8af76391c7d21aaab9e71
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2018-06-14 08:54:33 +0200

    Modified HeartbeatMessage

--- src/main/java/it/polimi/se2018/model/events/HeartbeatMessage.java
+++ src/main/java/it/polimi/se2018/model/events/HeartbeatMessage.java
@@ -3,10 +3,16 @@ package it.polimi.se2018.model.events;
 import it.polimi.se2018.network.visitor.MessageVisitorInterface;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
 
 public class HeartbeatMessage implements Message, Serializable {
 
     private int id;
+
+    private Instant sent;
+
+    private Instant received;
 
     public HeartbeatMessage(int id) {
         this.id = id;
@@ -19,5 +25,17 @@ public class HeartbeatMessage implements Message, Serializable {
 
     public int getId() {
         return id;
+    }
+
+    public void setReceived(Instant received) {
+        this.received = received;
+    }
+
+    public void setSent(Instant sent) {
+        this.sent = sent;
+    }
+
+    public long getTripDuration() {
+        return Duration.between(sent, received).toMillis();
     }
 }
