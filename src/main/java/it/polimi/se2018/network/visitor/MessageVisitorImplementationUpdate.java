ecb161e9812b02d3f201b7f832283a20c1be944a
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2018-06-29 17:24:32 +0200

    Use visitor for updatePlayers in Room

--- src/main/java/it/polimi/se2018/network/visitor/MessageVisitorImplementationUpdate.java
+++ src/main/java/it/polimi/se2018/network/visitor/MessageVisitorImplementationUpdate.java
@@ -1,22 +1,62 @@
 package it.polimi.se2018.network.visitor;
 
+import it.polimi.se2018.model.GamePhase;
 import it.polimi.se2018.model.events.*;
+import it.polimi.se2018.network.ConnectedClient;
 import it.polimi.se2018.network.server.Room;
 
-public class MessageVisitorDisconnectionImplementation implements MessageVisitorInterface {
+import java.rmi.RemoteException;
+
+public class MessageVisitorImplementationUpdate implements MessageVisitorInterface {
 
     private Room room;
 
-    public MessageVisitorDisconnectionImplementation(Room room) {
+    // player for which we're going to visit the message
+    private ConnectedClient player;
+
+    private int currentPlayerPlayingId;
+
+
+    public MessageVisitorImplementationUpdate(Room room) {
         this.room = room;
+    }
+
+    public void setPlayer(ConnectedClient player) {
+        this.player = player;
+    }
+
+    public void setCurrentPlayerPlayingId(int currentPlayerPlayingId) {
+        this.currentPlayerPlayingId = currentPlayerPlayingId;
+    }
+
+    private void update(ModelChangedMessage modelChangedMessage) {
+        try {
+            if(!player.isInactive())
+                player.getClientInterface().update(modelChangedMessage);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void visitModelChangedMessageRefresh(ModelChangedMessageRefresh modelChangedMessageRefresh) {
-        //System.out.println("visitor");
-        Integer id = Integer.parseInt(modelChangedMessageRefresh.getIdPlayerPlaying());
-        Message fakeMessage = new PlayerMessageEndTurn(id);
-        room.notifyView(fakeMessage);
+        if(this.player.isInactive()
+                && (this.player.getId() == this.currentPlayerPlayingId)
+                && modelChangedMessageRefresh.getGamePhase() == GamePhase.GAMEPHASE) {
+            Integer id = Integer.parseInt(modelChangedMessageRefresh.getIdPlayerPlaying());
+            Message fakeMessage = new PlayerMessageEndTurn(id);
+            this.room.notifyView(fakeMessage);
+        } else {
+            update(modelChangedMessageRefresh);
+        }
+    }
+
+
+    @Override
+    public void visitModelChangedMessagePlayerAFK(ModelChangedMessagePlayerAFK modelChangedMessagePlayerAFK) {
+        update(modelChangedMessagePlayerAFK);
+        if(modelChangedMessagePlayerAFK.getPlayer().equals(Integer.toString(player.getId())))
+            player.setInactive(true);
     }
 
     @Override
@@ -26,67 +66,62 @@ public class MessageVisitorDisconnectionImplementation implements MessageVisitor
 
     @Override
     public void visitModelChangedMessageConnected(ModelChangedMessageConnected modelChangedMessageConnected) {
-
+        update(modelChangedMessageConnected);
     }
 
     @Override
     public void visitModelChangedMessageDiceArena(ModelChangedMessageDiceArena modelChangedMessageDiceArena) {
-
+        update(modelChangedMessageDiceArena);
     }
 
     @Override
     public void visitModelChangedMessageDiceOnPatternCard(ModelChangedMessageDiceOnPatternCard modelChangedMessageDiceOnPatternCard) {
-
+        update(modelChangedMessageDiceOnPatternCard);
     }
 
     @Override
     public void visitModelChangedMessageEndGame(ModelChangedMessageEndGame modelChangedMessageEndGame) {
-
+        update(modelChangedMessageEndGame);
     }
 
     @Override
     public void visitModelChangedMessageMoveFailed(ModelChangedMessageMoveFailed modelChangedMessageMoveFailed) {
-
+        update(modelChangedMessageMoveFailed);
     }
 
     @Override
     public void visitModelChangedMessagePatternCard(ModelChangedMessagePatternCard modelChangedMessagePatternCard) {
-
+        update(modelChangedMessagePatternCard);
     }
 
     @Override
     public void visitModelChangedMessagePrivateObjective(ModelChangedMessagePrivateObjective modelChangedMessagePrivateObjective) {
-
+        update(modelChangedMessagePrivateObjective);
     }
 
     @Override
     public void visitModelChangedMessagePublicObjective(ModelChangedMessagePublicObjective modelChangedMessagePublicObjective) {
-
+        update(modelChangedMessagePublicObjective);
     }
 
     @Override
     public void visitModelChangedMessageRound(ModelChangedMessageRound modelChangedMessageRound) {
-
+        update(modelChangedMessageRound);
     }
 
     @Override
     public void visitModelChangedMessageNewEvent(ModelChangedMessageNewEvent modelChangedMessageNewEvent) {
-
+        update(modelChangedMessageNewEvent);
     }
 
     @Override
     public void visitModelChangedMessageToolCard(ModelChangedMessageToolCard modelChangedMessageToolCard) {
-
+        update(modelChangedMessageToolCard);
     }
 
     @Override
     public void visitModelChangedMessageTokensLeft(ModelChangedMessageTokensLeft modelChangedMessageTokensLeft) {
-
-    }
-
-    @Override
-    public void visitModelChangedMessagePlayerAFK(ModelChangedMessagePlayerAFK modelChangedMessagePlayerAFK) {
-
+        update(modelChangedMessageTokensLeft);
     }
 
     @Override
