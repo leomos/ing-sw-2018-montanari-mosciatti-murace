f674e54b236e3de1e90a5f97da794479c54365ae
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2018-06-29 15:52:33 +0200

    Implemented Singleton in Timer

--- src/main/java/it/polimi/se2018/model/Timer.java
+++ src/main/java/it/polimi/se2018/model/Timer.java
@@ -2,17 +2,36 @@ package it.polimi.se2018.model;
 
 import java.util.concurrent.TimeUnit;
 
-public class Timer extends Thread{
+public class Timer implements Runnable {
 
-    public static final int timer = 40;
+    /* Singleton implementation */
+    private static volatile Timer timerInstance;
+
+    private Timer() {
+        if(timerInstance != null) {
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public static Timer getInstance() {
+        if(timerInstance == null) {
+            synchronized (Timer.class) {
+                if(timerInstance == null) {
+                    timerInstance = new Timer();
+                }
+            }
+        }
+        return timerInstance;
+    }
+    /*end Singleton implementation */
+
+    private Thread thread;
+
+    public static final int timer = 20;
 
     private boolean stopTimer = false;
 
     private Model model;
-
-    public Timer(Model model){
-        this.model = model;
-    }
 
     @Override
     public void run(){
@@ -42,11 +61,24 @@ public class Timer extends Thread{
 
     }
 
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
     public void stopTimer(){
 
         stopTimer = true;
         System.out.println("ho stoppato il timer!");
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void startTimer() {
+        thread = new Thread(this);
+        thread.start();
     }
 
 
