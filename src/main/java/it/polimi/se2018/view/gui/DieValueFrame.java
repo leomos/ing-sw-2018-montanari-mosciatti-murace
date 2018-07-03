7834cb96aa698418cf8dd3e78c9ef15664f80303
Alessandro Murace <alessandro.murace@gmail.com>
2018-07-03 23:08:43 +0200

    Created DieValueFrame

---
+++ src/main/java/it/polimi/se2018/view/gui/DieValueFrame.java
@@ -0,0 +1,51 @@
+package it.polimi.se2018.view.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DieValueFrame {
+    private JDialog dialog;
+
+    private int value = 0;
+
+    public DieValueFrame() {
+        JFrame frame = new JFrame();
+        dialog = new JDialog(frame, "DIE VALUE", true);
+
+        JLabel label = new JLabel("Select a value", SwingConstants.CENTER);
+
+        JButton button = new JButton("OK");
+        button.setEnabled(false);
+        button.addActionListener(actionListener -> {
+                dialog.dispose();
+        });
+
+        ButtonGroup group = new ButtonGroup();
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.setPreferredSize(new Dimension(20, 100));
+        for (int i=0; i<6; i++) {
+            JRadioButton rb = new JRadioButton(Integer.toString(i+1));
+            group.add(rb);
+            panel.add(rb);
+            int finalI = i;
+            rb.addActionListener(actionListener -> {
+                    button.setEnabled(true);
+                    value = finalI + 1;
+            });
+        }
+
+        dialog.setLayout(new BorderLayout());
+        dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        dialog.setSize(100, 150);
+        dialog.add(label, BorderLayout.NORTH);
+        dialog.add(panel, BorderLayout.CENTER);
+        dialog.add(button, BorderLayout.SOUTH);
+        dialog.setVisible(true);
+    }
+
+    public int getValue() {
+        while (value==0);
+        return value;
+    }
+}
