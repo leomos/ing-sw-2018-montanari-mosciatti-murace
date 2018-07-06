7b175af174fc2b39118aa0a205284f3821c6b3f5
Mattereteo <matteo3.montanari@mail.polimi.it>
2018-07-06 23:24:00 +0200

    Number of rounds was 3

--- src/main/java/it/polimi/se2018/model/rounds/RoundTrack.java
+++ src/main/java/it/polimi/se2018/model/rounds/RoundTrack.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class RoundTrack {
 
-    public static final int NUMBER_OF_ROUNDS = 3;
+    public static final int NUMBER_OF_ROUNDS = 10;
 
     private ArrayList<Integer> players = new ArrayList<>();
 
